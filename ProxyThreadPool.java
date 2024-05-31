7582b009403204ff789fcf1fa065e193038e443b
Animesh Shukla <animeshshukla1518@gmail.com>
2024-05-31 20:07:21 +0530

    feat: Introduced thread pool

---
+++ ProxyThreadPool.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ProxyThreadPool {
+    private ThreadPoolExecutor threadPoolExecutor;
+
+    ProxyThreadPool(int corePoolSize, int maxPoolSize, long keepAliveTime) {
+        threadPoolExecutor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.MINUTES,
+                new LinkedBlockingQueue<Runnable>());
+    }
+
+    public void submitTask(Runnable task) {
+        int freeThreads = getFreeThreads();
+        System.out.println("free threads: " + freeThreads);
+        while (freeThreads == 0) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            freeThreads = getFreeThreads();
+        }
+        threadPoolExecutor.submit(task);
+    }
+
+    private int getFreeThreads() {
+        return threadPoolExecutor.getCorePoolSize() - threadPoolExecutor.getActiveCount();
+    }
+
+    public void shutdown() throws InterruptedException {
+        threadPoolExecutor.awaitTermination(1, TimeUnit.SECONDS);
+    }
+}
--- TCPServer.java
+++ TCPServer.java
@@ -6,8 +6,12 @@ public class TCPServer{
     public static void main(String args[]) throws IOException{
         Scanner sc=new Scanner(System.in);
         int serverPort=8080;
+        @SuppressWarnings("resource")
         ServerSocket serverSocket=new ServerSocket(serverPort);
         System.out.println("Server is listening on port: "+serverPort);
+
+        // spinning up a thread pool
+        ProxyThreadPool threadPool= new ProxyThreadPool(1,2,1L);
 
         while(true){
             Socket clientSocket=serverSocket.accept();
@@ -15,11 +19,8 @@ public class TCPServer{
             int clientPort=clientSocket.getPort();
             System.out.println("Client connected on"+clientIP+" "+clientPort);
 
-            
-
             // connecting to a destination server
-            new Thread(new DestHandler("localhost", 8081)).start();
-
+            threadPool.submitTask(new DestHandler("localhost", 8081));
 
             clientSocket.close();
             sc.close();
