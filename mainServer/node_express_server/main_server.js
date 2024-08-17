fa903075c68af87ca3171a98cf9c076de40e12cb
Animesh Shukla <animeshshukla1518@gmail.com>
2024-08-17 13:54:29 +0530

    feat: added multithreading in main js server using workerpool

--- mainServer/node_express_server/main_server.js
+++ mainServer/node_express_server/main_server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const workerpool = require('workerpool');
+const pool = workerpool.pool();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    setTimeout(() => {
-        console.log("GET request received");
-    console.log(req.headers);
-    console.log(req.query);
-    res.status(200).json({"message":"Hello World! after 5 seconds"});
-    }, 5000);
-    
+    pool.exec(heavyTask, [])
+        .then((message) => {
+            console.log("GET request received in workerpool");
+            res.status(200).json({ "message": message });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({ "error": "Workerpool error" });
+        });
 });
+
+function heavyTask() {
+    for (let i = 0; i < 10000000000; i++) {}
+    return "Hello World from Workerpool!";
+}
 
 app.post('/', (req, res) => {
     console.log("POST request received");
